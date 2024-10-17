2b5958df6181993d537ed0ac502a67e7d1a9458d
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2024-10-17 15:12:45 +0300

    [DOP-20135] Add search filter to dataset & job grids

--- src/components/dataset/DatasetGrid.tsx
+++ src/components/dataset/DatasetGrid.tsx
@@ -1,17 +1,36 @@
 import { ReactElement } from "react";
-import { List, Datagrid, TextField, WrapperField } from "react-admin";
+import {
+    List,
+    TextField,
+    WrapperField,
+    TopToolbar,
+    SelectColumnsButton,
+    DatagridConfigurable,
+    SearchInput,
+    minLength,
+} from "react-admin";
 import DatasetLocationIcon from "./DatasetLocationIcon";
+
+const DatasetGridActions = () => (
+    <TopToolbar>
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+
+const datasetFilters = [
+    <SearchInput source="search_query" alwaysOn validate={minLength(3)} />,
+];
 
 const DatasetGrid = (): ReactElement => {
     return (
-        <List>
-            <Datagrid bulkActionButtons={false}>
-                <WrapperField source="location.type">
+        <List actions={<DatasetGridActions />} filters={datasetFilters}>
+            <DatagridConfigurable bulkActionButtons={false}>
+                <WrapperField source="location.type" sortable={false}>
                     <DatasetLocationIcon />
                 </WrapperField>
-                <TextField source="location.name" />
-                <TextField source="name" />
-            </Datagrid>
+                <TextField source="location.name" sortable={false} />
+                <TextField source="name" sortable={false} />
+            </DatagridConfigurable>
         </List>
     );
 };
--- src/components/job/JobGrid.tsx
+++ src/components/job/JobGrid.tsx
@@ -1,21 +1,40 @@
 import { ReactElement } from "react";
-import { List, Datagrid, TextField, WrapperField } from "react-admin";
+import {
+    List,
+    TextField,
+    WrapperField,
+    TopToolbar,
+    SelectColumnsButton,
+    SearchInput,
+    minLength,
+    DatagridConfigurable,
+} from "react-admin";
 import JobIcon from "./JobIcon";
 import JobLocationIcon from "./JobLocationIcon";
 
+const JobGridActions = () => (
+    <TopToolbar>
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+
+const jobFilters = [
+    <SearchInput source="search_query" alwaysOn validate={minLength(3)} />,
+];
+
 const JobGrid = (): ReactElement => {
     return (
-        <List>
-            <Datagrid bulkActionButtons={false}>
-                <WrapperField source="type">
+        <List actions={<JobGridActions />} filters={jobFilters}>
+            <DatagridConfigurable bulkActionButtons={false}>
+                <WrapperField source="type" sortable={false}>
                     <JobIcon />
                 </WrapperField>
-                <TextField source="name" />
-                <WrapperField source="location.type">
+                <TextField source="name" sortable={false} />
+                <WrapperField source="location.type" sortable={false}>
                     <JobLocationIcon />
                 </WrapperField>
-                <TextField source="location.name" />
-            </Datagrid>
+                <TextField source="location.name" sortable={false} />
+            </DatagridConfigurable>
         </List>
     );
 };
