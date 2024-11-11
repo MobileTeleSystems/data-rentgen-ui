419ce262f80efc41ae12cf5152fbd8dc508b89c3
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2024-11-11 17:19:22 +0300

    [DOP-20148] Fix image

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/bin/env sh
 
 # replace api url with one specified in env variable
 sed s#http://localhost:8000#${DATA_RENTGEN__UI__API_BROWSER_URL}# -i /usr/share/nginx/html/assets/index*.js
