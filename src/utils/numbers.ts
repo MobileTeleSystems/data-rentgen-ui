54acc7a1edef32ddc1d0ea677ca45efe83ad1171
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2024-11-21 17:15:05 +0300

    [DOP-21357] Fix formatNumberApprox

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,14 +1,14 @@
 import { useTranslate } from "react-admin";
 
 const step = 1000;
-const suffixes = ["K", "M", "B", "T"];
+const suffixes = ["", "K", "M", "B", "T"];
 
 const formatNumberApprox = (value: number, decimals = 2): string => {
     const translate = useTranslate();
     if (value < step) return value.toString();
 
     const i = Math.floor(Math.log(value) / Math.log(step));
-    const suffix = suffixes[i - 1];
+    const suffix = suffixes[i];
     const suffixTranslation = translate(`units.numbers.${suffix}`, {
         _: suffix,
     });
