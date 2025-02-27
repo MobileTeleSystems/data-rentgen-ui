10f1de8721b75d4d1d442c00a8763935a296e6ec
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-02-27 17:25:28 +0300

    [DOP-22580] Add ES2015 polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 import webfontDownload from "vite-plugin-webfont-dl";
 import svgr from "vite-plugin-svgr";
 import path from "path";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,6 +19,7 @@ export default defineConfig({
         ]),
         react(),
         alias(),
+        nodePolyfills(),
         svgr({
             svgrOptions: {
                 plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
@@ -49,6 +51,7 @@ export default defineConfig({
         keepNames: true,
     },
     build: {
+        target: "es2015",
         sourcemap: true,
     },
 });
