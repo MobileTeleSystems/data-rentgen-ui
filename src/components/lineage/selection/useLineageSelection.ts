e2d0be1d2d19bd342ad13821f00d55674f97baac
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-02-27 18:40:19 +0300

    [DOP-22580] Improve selection algorithm

--- src/components/lineage/selection/useLineageSelection.ts
+++ src/components/lineage/selection/useLineageSelection.ts
@@ -2,7 +2,8 @@ import { Edge, Node, useReactFlow } from "@xyflow/react";
 import { useCallback, useState, MouseEvent } from "react";
 
 const useLineageSelection = () => {
-    const { getNodes, getEdges, setEdges, setNodes } = useReactFlow();
+    const { getNodes, getEdges, setEdges, setNodes, getNodeConnections } =
+        useReactFlow();
     const [hideNonSelected, setHideNonSelected] = useState(false);
 
     const setSelection = (
@@ -27,46 +28,84 @@ const useLineageSelection = () => {
         );
     };
 
-    const trackUpstreams = (
-        nodeId: string,
+    const getAllRelations = (
+        nodeIds: string[],
     ): { nodes: Set<string>; edges: Set<string> } => {
+        const intermediate = {
+            upstreamNodes: new Set<string>(nodeIds),
+            downstreamNodes: new Set<string>(nodeIds),
+            upstreamEdges: new Set<string>(),
+            downstreamEdges: new Set<string>(),
+        };
+
+        const rawEdges = getEdges();
+
+        const edgesBySource = new Map<string, Edge[]>();
+        const edgesByTarget = new Map<string, Edge[]>();
+        rawEdges.forEach((edge) => {
+            const sameSource = edgesBySource.get(edge.source) ?? [];
+            sameSource.push(edge);
+            edgesBySource.set(edge.source, sameSource);
+
+            const sameTarget = edgesByTarget.get(edge.target) ?? [];
+            sameTarget.push(edge);
+            edgesByTarget.set(edge.target, sameTarget);
+        });
+
+        while (edgesByTarget.size > 0) {
+            let reachedAnything = false;
+            intermediate.upstreamNodes.forEach((nodeId) => {
+                const upstreamEdges = edgesByTarget.get(nodeId);
+                if (upstreamEdges) {
+                    upstreamEdges.forEach((edge: Edge) => {
+                        intermediate.upstreamNodes.add(edge.source);
+                        intermediate.upstreamEdges.add(edge.id);
+                    });
+                    edgesByTarget.delete(nodeId);
+                    reachedAnything = true;
+                }
+            });
+            if (!reachedAnything) {
+                break;
+            }
+        }
+
+        while (edgesBySource.size > 0) {
+            let reachedAnything = false;
+            intermediate.downstreamNodes.forEach((nodeId) => {
+                const downstreamEdges = edgesBySource.get(nodeId);
+                if (downstreamEdges) {
+                    downstreamEdges.forEach((edge: Edge) => {
+                        intermediate.downstreamNodes.add(edge.target);
+                        intermediate.downstreamEdges.add(edge.id);
+                    });
+                    edgesBySource.delete(nodeId);
+                    reachedAnything = true;
+                }
+            });
+            if (!reachedAnything) {
+                break;
+            }
+        }
+
         const result = {
-            nodes: new Set<string>([nodeId]),
+            nodes: new Set<string>(),
             edges: new Set<string>(),
         };
 
-        getEdges()
-            .filter((edge) => edge.target == nodeId)
-            .forEach((edge) => {
-                result.nodes.add(edge.source);
-                result.edges.add(edge.id);
-
-                const { nodes, edges } = trackUpstreams(edge.source);
-                nodes.forEach((node) => result.nodes.add(node));
-                edges.forEach((edge) => result.edges.add(edge));
-            });
-
-        return result;
-    };
-
-    const trackDownstreams = (
-        nodeId: string,
-    ): { nodes: Set<string>; edges: Set<string> } => {
-        const result = {
-            nodes: new Set<string>([nodeId]),
-            edges: new Set<string>(),
-        };
-
-        getEdges()
-            .filter((edge) => edge.source == nodeId)
-            .forEach((edge) => {
-                result.nodes.add(edge.target);
-                result.edges.add(edge.id);
-
-                const { nodes, edges } = trackDownstreams(edge.target);
-                nodes.forEach((node) => result.nodes.add(node));
-                edges.forEach((edge) => result.edges.add(edge));
-            });
+        // Set union is only in ES2024
+        intermediate.upstreamNodes.forEach((nodeId) =>
+            result.nodes.add(nodeId),
+        );
+        intermediate.upstreamEdges.forEach((edgeId) =>
+            result.edges.add(edgeId),
+        );
+        intermediate.downstreamNodes.forEach((nodeId) =>
+            result.nodes.add(nodeId),
+        );
+        intermediate.downstreamEdges.forEach((edgeId) =>
+            result.edges.add(edgeId),
+        );
 
         return result;
     };
@@ -81,18 +120,11 @@ const useLineageSelection = () => {
 
     const onEdgeDoubleClick = useCallback(
         (e: MouseEvent, currentEdge: Edge) => {
-            const upstreams = trackUpstreams(currentEdge.source);
-            const downstreams = trackDownstreams(currentEdge.target);
-
-            // Set.union is onl in ES2024
-            const relatedNodes = upstreams.nodes;
-            downstreams.nodes.forEach((node) => relatedNodes.add(node));
-
-            const relatedEdges = upstreams.edges;
-            downstreams.edges.forEach((edges) => relatedEdges.add(edges));
-            relatedEdges.add(currentEdge.id);
-
-            setSelection(relatedNodes, relatedEdges);
+            const { nodes, edges } = getAllRelations([
+                currentEdge.source,
+                currentEdge.target,
+            ]);
+            setSelection(nodes, edges);
             e.stopPropagation();
         },
         [],
@@ -106,18 +138,8 @@ const useLineageSelection = () => {
 
     const onNodeDoubleClick = useCallback(
         (e: MouseEvent, currentNode: Node) => {
-            const upstreams = trackUpstreams(currentNode.id);
-            const downstreams = trackDownstreams(currentNode.id);
-
-            // Set.union is onl in ES2024
-            const relatedNodes = upstreams.nodes;
-            downstreams.nodes.forEach((node) => relatedNodes.add(node));
-            relatedNodes.add(currentNode.id);
-
-            const relatedEdges = upstreams.edges;
-            downstreams.edges.forEach((edges) => relatedEdges.add(edges));
-
-            setSelection(relatedNodes, relatedEdges);
+            const { nodes, edges } = getAllRelations([currentNode.id]);
+            setSelection(nodes, edges);
             e.stopPropagation();
         },
         [],
