c2f392a1198a974beca14909ac0224a7f5b63f91
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-02-28 17:07:57 +0300

    [DOP-23920] Cache lineage selection functions

--- src/components/lineage/selection/useLineageSelection.ts
+++ src/components/lineage/selection/useLineageSelection.ts
@@ -1,101 +1,110 @@
 import { Edge, Node, useReactFlow } from "@xyflow/react";
 import { useCallback, MouseEvent } from "react";
 
+const getAllConnection = (
+    startNodeIds: string[],
+    edgesByNodeId: Map<string, Edge[]>,
+    edgeAttribute: "source" | "target",
+): { nodes: Set<string>; edges: Set<string> } => {
+    const result = {
+        nodes: new Set<string>(),
+        edges: new Set<string>(),
+    };
+
+    let startFrom = new Set<string>(startNodeIds);
+    while (edgesByNodeId.size > 0) {
+        const visitedNodes = new Set<string>();
+        const visitedEdges = new Set<string>();
+
+        startFrom.forEach((nodeId) => {
+            const edges = edgesByNodeId.get(nodeId);
+            if (edges) {
+                edges.forEach((edge: Edge) => {
+                    visitedNodes.add(edge[edgeAttribute]);
+                    visitedEdges.add(edge.id);
+                });
+                edgesByNodeId.delete(nodeId);
+            }
+        });
+        if (visitedNodes.size == 0 && visitedEdges.size == 0) {
+            break;
+        }
+
+        visitedNodes.forEach((nodeId) => {
+            result.nodes.add(nodeId);
+        });
+        visitedEdges.forEach((edgeId) => {
+            result.edges.add(edgeId);
+        });
+        startFrom = visitedNodes;
+    }
+
+    return result;
+};
+
 const useLineageSelection = () => {
     const { getNodes, getEdges, setEdges, setNodes } = useReactFlow();
 
-    const setSelection = (
-        nodesToSelect: Set<string>,
-        edgesToSelect: Set<string>,
-    ) => {
-        const nodes = getNodes().map((node) => {
-            node.selected = nodesToSelect.has(node.id);
-            return node;
-        });
-
-        const edges = getEdges().map((edge) => {
-            edge.selected = edgesToSelect.has(edge.id);
-            return edge;
-        });
-
-        setNodes(nodes);
-        setEdges(edges);
-    };
-
-    const _visitNodes = (
-        startNodeIds: string[],
-        edgesByNodeId: Map<string, Edge[]>,
-        edgeAttribute: "source" | "target",
-    ): { nodes: Set<string>; edges: Set<string> } => {
-        const result = {
-            nodes: new Set<string>(),
-            edges: new Set<string>(),
-        };
-
-        let startFrom = new Set<string>(startNodeIds);
-        while (edgesByNodeId.size > 0) {
-            const visitedNodes = new Set<string>();
-            const visitedEdges = new Set<string>();
-
-            startFrom.forEach((nodeId) => {
-                const edges = edgesByNodeId.get(nodeId);
-                if (edges) {
-                    edges.forEach((edge: Edge) => {
-                        visitedNodes.add(edge[edgeAttribute]);
-                        visitedEdges.add(edge.id);
-                    });
-                    edgesByNodeId.delete(nodeId);
-                }
+    const setSelection = useCallback(
+        (nodesToSelect: Set<string>, edgesToSelect: Set<string>) => {
+            const nodes = getNodes().map((node) => {
+                node.selected = nodesToSelect.has(node.id);
+                return node;
             });
-            if (visitedNodes.size == 0 && visitedEdges.size == 0) {
-                break;
-            }
 
-            visitedNodes.forEach((nodeId) => {
-                result.nodes.add(nodeId);
+            const edges = getEdges().map((edge) => {
+                edge.selected = edgesToSelect.has(edge.id);
+                return edge;
             });
-            visitedEdges.forEach((edgeId) => {
-                result.edges.add(edgeId);
+
+            setNodes(nodes);
+            setEdges(edges);
+        },
+        [],
+    );
+
+    const getAllRelations = useCallback(
+        (nodeIds: string[]): { nodes: Set<string>; edges: Set<string> } => {
+            const result = {
+                nodes: new Set<string>(nodeIds),
+                edges: new Set<string>(),
+            };
+
+            const rawEdges = getEdges();
+
+            // Convert list to maps to use O(1) lookups
+            const edgesBySource = new Map<string, Edge[]>();
+            const edgesByTarget = new Map<string, Edge[]>();
+            rawEdges.forEach((edge) => {
+                const sameSource = edgesBySource.get(edge.source) ?? [];
+                sameSource.push(edge);
+                edgesBySource.set(edge.source, sameSource);
+
+                const sameTarget = edgesByTarget.get(edge.target) ?? [];
+                sameTarget.push(edge);
+                edgesByTarget.set(edge.target, sameTarget);
             });
-            startFrom = visitedNodes;
-        }
 
-        return result;
-    };
+            const upstreams = getAllConnection(
+                nodeIds,
+                edgesByTarget,
+                "source",
+            );
+            const downstreams = getAllConnection(
+                nodeIds,
+                edgesBySource,
+                "target",
+            );
 
-    const getAllRelations = (
-        nodeIds: string[],
-    ): { nodes: Set<string>; edges: Set<string> } => {
-        const result = {
-            nodes: new Set<string>(nodeIds),
-            edges: new Set<string>(),
-        };
+            upstreams.nodes.forEach((nodeId) => result.nodes.add(nodeId));
+            upstreams.edges.forEach((edgeId) => result.edges.add(edgeId));
+            downstreams.nodes.forEach((nodeId) => result.nodes.add(nodeId));
+            downstreams.edges.forEach((edgeId) => result.edges.add(edgeId));
 
-        const rawEdges = getEdges();
-
-        // Convert list to maps to use O(1) lookups
-        const edgesBySource = new Map<string, Edge[]>();
-        const edgesByTarget = new Map<string, Edge[]>();
-        rawEdges.forEach((edge) => {
-            const sameSource = edgesBySource.get(edge.source) ?? [];
-            sameSource.push(edge);
-            edgesBySource.set(edge.source, sameSource);
-
-            const sameTarget = edgesByTarget.get(edge.target) ?? [];
-            sameTarget.push(edge);
-            edgesByTarget.set(edge.target, sameTarget);
-        });
-
-        const upstreams = _visitNodes(nodeIds, edgesByTarget, "source");
-        const downstreams = _visitNodes(nodeIds, edgesBySource, "target");
-
-        upstreams.nodes.forEach((nodeId) => result.nodes.add(nodeId));
-        upstreams.edges.forEach((edgeId) => result.edges.add(edgeId));
-        downstreams.nodes.forEach((nodeId) => result.nodes.add(nodeId));
-        downstreams.edges.forEach((edgeId) => result.edges.add(edgeId));
-
-        return result;
-    };
+            return result;
+        },
+        [],
+    );
 
     const selectNode = useCallback((nodeId: string) => {
         setSelection(new Set([nodeId]), new Set());
@@ -141,6 +150,7 @@ const useLineageSelection = () => {
     }, []);
 
     return {
+        setSelection,
         onEdgeClick,
         onEdgeDoubleClick,
         onNodeClick,
