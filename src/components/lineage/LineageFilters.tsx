5ed351f63a5d18202877032622010757839edd4e
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-03-18 10:55:05 +0300

    [DOP-22363] Increase max lineage depth to 10

--- src/components/lineage/LineageFilters.tsx
+++ src/components/lineage/LineageFilters.tsx
@@ -120,6 +120,34 @@ const LineageFilters = ({
                                     id: 3,
                                     name: "3",
                                 },
+                                {
+                                    id: 4,
+                                    name: "4",
+                                },
+                                {
+                                    id: 5,
+                                    name: "5",
+                                },
+                                {
+                                    id: 6,
+                                    name: "6",
+                                },
+                                {
+                                    id: 7,
+                                    name: "7",
+                                },
+                                {
+                                    id: 8,
+                                    name: "8",
+                                },
+                                {
+                                    id: 9,
+                                    name: "9",
+                                },
+                                {
+                                    id: 10,
+                                    name: "10",
+                                },
                             ]}
                             defaultValue={1}
                             validate={required()}
