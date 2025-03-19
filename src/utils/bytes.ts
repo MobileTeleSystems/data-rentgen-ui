053976da54485b43cae32fa2cff04c6919dc3b08
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-03-19 12:58:49 +0300

    [DOP-24570] Round byte size

--- src/utils/bytes.ts
+++ src/utils/bytes.ts
@@ -5,10 +5,18 @@ const suffixes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
 const formatBytes = (bytes: number, precision = 3) => {
     const translate = useTranslate();
-    const i = Math.floor(Math.log(bytes) / Math.log(step));
+    let i = Math.floor(Math.log(bytes) / Math.log(step));
+    let result = bytes / Math.pow(step, i);
+
+    if (result >= 1000 && result < step) {
+        // Avoid results like "1.01e+3MB"
+        i += 1;
+        result = 1;
+        precision = 1;
+    }
+
     const suffix = suffixes[i];
     const suffixTranslation = translate(`units.bytes.${suffix}`, { _: suffix });
-    const result = bytes / Math.pow(step, i);
 
     return result.toPrecision(precision) + suffixTranslation;
 };
