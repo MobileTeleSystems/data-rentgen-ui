96f234fd9bae7127bf1fee7e419e6d382b03293e
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-06-20 18:29:59 +0300

    [DOP-26447] Add Hive support

--- src/components/job/JobIcon.tsx
+++ src/components/job/JobIcon.tsx
@@ -1,27 +1,9 @@
 import { ReactElement } from "react";
-import {
-    ApacheAirflowIcon,
-    ApacheSparkIcon,
-    ApacheFlinkIcon,
-    DBTIcon,
-} from "@/components/icons";
+import { IconByName } from "@/components/icons";
 import { JobResponseV1 } from "@/dataProvider/types";
-import { QuestionMark } from "@mui/icons-material";
 
 const JobIcon = ({ job }: { job: JobResponseV1 }): ReactElement => {
-    if (job.type.startsWith("SPARK")) {
-        return <ApacheSparkIcon />;
-    }
-    if (job.type.startsWith("AIRFLOW")) {
-        return <ApacheAirflowIcon />;
-    }
-    if (job.type.startsWith("FLINK")) {
-        return <ApacheFlinkIcon />;
-    }
-    if (job.type.startsWith("DBT")) {
-        return <DBTIcon />;
-    }
-    return <QuestionMark />;
+    return <IconByName name={job.type} />;
 };
 
 export default JobIcon;
