51ef9b8469d79a8264fa1fc57b55ca68806abdef
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-07-01 19:16:05 +0300

    [DOP-25363] Move SQL query field to another column

--- src/components/operation/OperationRaShow.tsx
+++ src/components/operation/OperationRaShow.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Stack, Grid } from "@mui/material";
 import { ReactElement } from "react";
 import {
     DateField,
@@ -15,107 +15,124 @@ import {
     IOStatisticsField,
     StatusRaField,
 } from "@/components/base";
-import { Box } from "@mui/material";
 import OperationRaLineage from "./OperationRaLineage";
 import { OperationDetailedResponseV1 } from "@/dataProvider/types";
 
 const OperationRaShow = (): ReactElement => {
     return (
         <Show>
-            <SimpleShowLayout>
-                <TextField source="id" label="resources.operations.fields.id" />
-                <TextField
-                    source="data.name"
-                    label="resources.operations.fields.name"
-                />
+            <Grid container spacing={2}>
+                <Grid item>
+                    <SimpleShowLayout>
+                        <TextField
+                            source="id"
+                            label="resources.operations.fields.id"
+                        />
+                        <TextField
+                            source="data.name"
+                            label="resources.operations.fields.name"
+                        />
 
-                <Labeled label="resources.operations.sections.external">
-                    <Stack direction="row" spacing={3}>
-                        <Labeled label="resources.operations.sections.position">
-                            <TextField source="data.position" />
+                        <Labeled label="resources.operations.sections.external">
+                            <Stack direction="row" spacing={3}>
+                                <Labeled label="resources.operations.sections.position">
+                                    <TextField source="data.position" />
+                                </Labeled>
+                                <Labeled label="resources.operations.sections.group">
+                                    <TextField source="data.group" />
+                                </Labeled>
+                                <Labeled label="resources.operations.sections.description">
+                                    <TextField source="data.description" />
+                                </Labeled>
+                            </Stack>
                         </Labeled>
-                        <Labeled label="resources.operations.sections.group">
-                            <TextField source="data.group" />
-                        </Labeled>
-                        <Labeled label="resources.operations.sections.description">
-                            <TextField source="data.description" />
-                        </Labeled>
-                    </Stack>
-                </Labeled>
 
-                <Labeled label="resources.operations.sections.created">
-                    <Stack direction="row" spacing={3}>
-                        <Labeled label="resources.operations.sections.when">
-                            <DateField
-                                source="data.created_at"
-                                showTime={true}
-                            />
+                        <Labeled label="resources.operations.sections.created">
+                            <Stack direction="row" spacing={3}>
+                                <Labeled label="resources.operations.sections.when">
+                                    <DateField
+                                        source="data.created_at"
+                                        showTime={true}
+                                    />
+                                </Labeled>
+                                <Labeled label="resources.operations.sections.by_run">
+                                    <ReferenceField
+                                        source="data.run_id"
+                                        reference="runs"
+                                    />
+                                </Labeled>
+                            </Stack>
                         </Labeled>
-                        <Labeled label="resources.operations.sections.by_run">
-                            <ReferenceField
-                                source="data.run_id"
-                                reference="runs"
-                            />
-                        </Labeled>
-                    </Stack>
-                </Labeled>
 
-                <DateField
-                    source="data.started_at"
-                    label="resources.operations.fields.started_at"
-                    showTime={true}
-                />
+                        <DateField
+                            source="data.started_at"
+                            label="resources.operations.fields.started_at"
+                            showTime={true}
+                        />
 
-                <StatusRaField source="status" />
-                <Labeled label="resources.operations.sections.ended">
-                    <Stack direction="row" spacing={3}>
-                        <Labeled label="resources.operations.sections.when">
-                            <DateField source="data.ended_at" showTime={true} />
+                        <StatusRaField source="status" />
+                        <Labeled label="resources.operations.sections.ended">
+                            <Stack direction="row" spacing={3}>
+                                <Labeled label="resources.operations.sections.when">
+                                    <DateField
+                                        source="data.ended_at"
+                                        showTime={true}
+                                    />
+                                </Labeled>
+                                <Labeled label="resources.operations.sections.duration">
+                                    <DurationRaField source="duration" />
+                                </Labeled>
+                            </Stack>
                         </Labeled>
-                        <Labeled label="resources.operations.sections.duration">
-                            <DurationRaField source="duration" />
+                        <Labeled label="resources.operations.sections.statistics.name">
+                            <Stack direction="row" spacing={3}>
+                                <Labeled label="resources.operations.sections.statistics.inputs">
+                                    <IOStatisticsField source="statistics.inputs" />
+                                </Labeled>
+                                <Labeled label="resources.operations.sections.statistics.outputs">
+                                    <IOStatisticsField source="statistics.outputs" />
+                                </Labeled>
+                            </Stack>
                         </Labeled>
-                    </Stack>
-                </Labeled>
+                    </SimpleShowLayout>
+                </Grid>
 
-                <Labeled label="resources.operations.sections.statistics.name">
-                    <Stack direction="row" spacing={3}>
-                        <Labeled label="resources.operations.sections.statistics.inputs">
-                            <IOStatisticsField source="statistics.inputs" />
+                <Grid item>
+                    <SimpleShowLayout>
+                        <Labeled label="resources.operations.sections.sql_query">
+                            <Stack
+                                direction="row"
+                                spacing={3}
+                                sx={{
+                                    maxHeight: "35en",
+                                    maxWidth: "35em",
+                                    overflowY: "auto",
+                                    overflowX: "auto",
+                                }}
+                            >
+                                <TextField
+                                    source="data.sql_query"
+                                    component="pre"
+                                />
+                            </Stack>
                         </Labeled>
-                        <Labeled label="resources.operations.sections.statistics.outputs">
-                            <IOStatisticsField source="statistics.outputs" />
-                        </Labeled>
-                    </Stack>
-                </Labeled>
+                    </SimpleShowLayout>
+                </Grid>
+            </Grid>
 
-                <Labeled label="resources.operations.sections.sql_query">
-                    <Stack
-                        direction="row"
-                        spacing={3}
-                        sx={{
-                            height: "10rem",
-                            overflowY: "auto",
-                            overflowX: "auto",
-                        }}
-                    >
-                        <TextField source="data.sql_query" component="pre" />
-                    </Stack>
-                </Labeled>
-                <FunctionField
-                    render={(record: OperationDetailedResponseV1) => {
-                        return record.statistics.inputs.total_datasets +
-                            record.statistics.outputs.total_datasets >
-                            0 ? (
-                            <TabbedShowLayout>
-                                <TabbedShowLayout.Tab label="resources.operations.tabs.lineage">
-                                    <OperationRaLineage />
-                                </TabbedShowLayout.Tab>
-                            </TabbedShowLayout>
-                        ) : null;
-                    }}
-                />
-            </SimpleShowLayout>
+            <FunctionField
+                render={(record: OperationDetailedResponseV1) => {
+                    return record.statistics.inputs.total_datasets +
+                        record.statistics.outputs.total_datasets >
+                        0 ? (
+                        <TabbedShowLayout>
+                            <TabbedShowLayout.Tab label="resources.operations.tabs.lineage">
+                                <OperationRaLineage />
+                            </TabbedShowLayout.Tab>
+                        </TabbedShowLayout>
+                    ) : null;
+                }}
+            />
         </Show>
     );
 };
