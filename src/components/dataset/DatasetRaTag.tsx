ac8b1dbb33e9864ad43552369b4da413965976ea
Якименков Кирилл Александрович <kayakimenk@mts.ru>
2025-07-23 16:11:43 +0300

    [DOP-27824] add types

--- src/components/dataset/DatasetRaTag.tsx
+++ src/components/dataset/DatasetRaTag.tsx
@@ -1,5 +1,6 @@
 import { useRecordContext } from "react-admin";
 import { Stack, Chip, Box } from "@mui/material";
+import { Key } from "react";
 
 const DatasetRaTag = () => {
     const record = useRecordContext();
@@ -8,16 +9,18 @@ const DatasetRaTag = () => {
     }
     return (
         <Stack spacing={1}>
-            {record.data.tags.map((tag, index) => (
-                <Box key={index} sx={{ width: "fit-content" }}>
-                    <Chip
-                        label={`${tag.name}: ${tag.value}`}
-                        size="small"
-                        variant="outlined"
-                        sx={{ fontSize: "0.7rem" }}
-                    ></Chip>
-                </Box>
-            ))}
+            {record.data.tags.map(
+                (tag: { name: string; value: string }, index: Key) => (
+                    <Box key={index} sx={{ width: "fit-content" }}>
+                        <Chip
+                            label={`${tag.name}: ${tag.value}`}
+                            size="small"
+                            variant="outlined"
+                            sx={{ fontSize: "0.7rem" }}
+                        ></Chip>
+                    </Box>
+                ),
+            )}
         </Stack>
     );
 };
